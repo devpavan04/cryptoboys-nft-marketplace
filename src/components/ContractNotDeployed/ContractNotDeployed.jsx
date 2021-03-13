c50c876ff86e13fe073666869fda37f3f1f7b48b
pavanvsoratur <pavanvsoratur@gmail.com>
2021-03-14 00:38:06 +0530

    change in abi

--- src/components/ContractNotDeployed/ContractNotDeployed.jsx
+++ src/components/ContractNotDeployed/ContractNotDeployed.jsx
@@ -6,7 +6,7 @@ const ContractNotDeployed = () => {
       <h3>Crypto Boys Contract Not Deployed To This Network.</h3>
       <hr className="my-4" />
       <p className="lead">
-        Connect Metamask to Kovan Testnet Or localhost : 7545 running a custom RPC
+        Connect Metamask to Kovan Testnet Or Localhost 7545 running a custom RPC
         like Ganache.
       </p>
     </div>
