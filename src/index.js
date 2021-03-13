f46f83feb6c1cbea52b9eb4d6bfd4d83818f504c
pavanvsoratur <pavanvsoratur@gmail.com>
2021-03-13 17:48:27 +0530

    imported hash-router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { HashRouter } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
 
 import "./bootstrap.min.css";
