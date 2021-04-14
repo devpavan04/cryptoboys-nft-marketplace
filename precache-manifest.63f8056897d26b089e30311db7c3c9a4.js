8813f724e6aee0aa1b3996868121a6e04451634d
pavanvsoratur <pavanvsoratur@gmail.com>
2021-04-14 10:56:26 +0530

    Updates

---
+++ precache-manifest.63f8056897d26b089e30311db7c3c9a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "613455d5a0362ca81b74",
+    "url": "/cryptoboys-nft-marketplace/static/css/main.16c55b98.chunk.css"
+  },
+  {
+    "revision": "613455d5a0362ca81b74",
+    "url": "/cryptoboys-nft-marketplace/static/js/main.613455d5.chunk.js"
+  },
+  {
+    "revision": "b24a269478d557134c4f",
+    "url": "/cryptoboys-nft-marketplace/static/js/1.b24a2694.chunk.js"
+  },
+  {
+    "revision": "8bf4deafcc65d566b658",
+    "url": "/cryptoboys-nft-marketplace/static/js/runtime~main.8bf4deaf.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/cryptoboys-nft-marketplace/static/media/loading.26ef8bb4.gif"
+  },
+  {
+    "revision": "fbf33967fa244d21d61fb85f233fc331",
+    "url": "/cryptoboys-nft-marketplace/static/media/metamask.fbf33967.svg"
+  },
+  {
+    "revision": "8036a52718908272004ac489f7c0b6eb",
+    "url": "/cryptoboys-nft-marketplace/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/cryptoboys-nft-marketplace/precache-manifest.63f8056897d26b089e30311db7c3c9a4.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/cryptoboys-nft-marketplace/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
